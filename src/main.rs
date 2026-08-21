6fee438131cdbc7a0cf4f83cff9bef6748a80f42
Nikita <80875524+enarve@users.noreply.github.com>
2026-08-21 18:23:26 +0200

    Switched to daemon to handle multiple windows

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![windows_subsystem = "windows"] // windows-specific: hide default terminal window
 
-use std::println;
 use std::time::{Instant, Duration};
 
 use iced::widget::{container, mouse_area};
@@ -9,23 +8,13 @@ use iced::window::{self, Mode};
 
 use crate::State::{Breaking, Working};
 
-const WORK_DURATION: Duration = Duration::from_secs(15); // short period for development
-const BREAK_DURATION: Duration = Duration::from_secs(10);
-
 fn main() -> iced::Result {
-    let window_settings = window::Settings {
-        size: Size::new(250.0, 100.0),
-        position: window::Position::Centered,
-        decorations: false,
-        transparent: true,
-        resizable: false,
-        visible: false,
-        level: window::Level::AlwaysOnTop,
+    let daemon_settings = iced::Settings {
         ..Default::default()
     };
-    
-    iced::application(App::default, update, view)
-        .window(window_settings)
+
+    iced::daemon(boot, update, view)
+        .settings(daemon_settings)
         .style(|_state, _theme| iced::theme::Style {
             background_color: Color::TRANSPARENT, // Removes default canvas layer
             text_color: Color::WHITE,
@@ -36,8 +25,45 @@ fn main() -> iced::Result {
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
+    Initialized(window::Id),
     WindowDragged,
-    Tick(Instant)
+    Tick(Instant),
+    OpenSettings,
+    CloseSettings,
+}
+
+struct Config {
+    work_duration: Duration,
+    break_duration: Duration,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            work_duration: Duration::from_secs(15),
+            break_duration: Duration::from_secs(10)
+        }
+    }
+}
+
+struct SettingsInputBuffer {
+    work_duration_input: String,
+    break_duration_input: String,
+}
+
+impl SettingsInputBuffer {
+    fn from_config(config: &Config) -> Self {
+        Self {
+            work_duration_input: config.work_duration.as_secs().to_string(),
+            break_duration_input: config.break_duration.as_secs().to_string(),
+        }
+    }
+}
+
+impl Default for SettingsInputBuffer {
+    fn default() -> Self {
+        SettingsInputBuffer::from_config(&Config::default())
+    }
 }
 
 enum State {
@@ -45,19 +71,61 @@ enum State {
     Breaking(Instant)
 }
 
+struct Windows {
+    main: Option<window::Id>,
+    settings: Option<window::Id>,
+}
+
+impl Default for Windows {
+    fn default() -> Self {
+        Self {
+            main: None,
+            settings: None,
+        }
+    }
+}
+
 struct App {
-    state: State
+    state: State,
+    windows: Windows,
+    config: Config,
+    settings_input_buffer: Option<SettingsInputBuffer>,
 }
 
 impl Default for App {
     fn default() -> Self {
         let state = Working(Instant::now());
-        App { state }
+        App {
+            state,
+            windows: Windows::default(),
+            config: Config::default(),
+            settings_input_buffer: Option::None,
+        }
     }
+}
+
+fn boot() -> (App, Task<Message>) {
+    let app = App::default();
+    
+    let main_window_settings = window::Settings {
+        size: Size::new(250.0, 100.0),
+        position: window::Position::Centered,
+        decorations: false,
+        transparent: true,
+        resizable: false,
+        visible: false,
+        level: window::Level::AlwaysOnTop,
+        ..Default::default()
+    };
+    let (main_id, open_task) = window::open(main_window_settings);
+    let initial_task = open_task.map(move |_| Message::Initialized(main_id));
+    
+    (app, initial_task)
 }
 
 fn update(app: &mut App, message: Message) -> iced::Task<Message> {
     match message {
+        Message::Initialized(_) => { Task::none() },
         Message::WindowDragged => {
             window::oldest().and_then(window::drag)
         },
@@ -66,21 +134,21 @@ fn update(app: &mut App, message: Message) -> iced::Task<Message> {
                 
                 Working(start_time) => {
                     let elapsed = now.duration_since(start_time);
+                    println!("{:?}", elapsed);
                     
-                    if elapsed > WORK_DURATION {
+                    if elapsed > app.config.work_duration {
                         // show mouse
                         app.state = Breaking(Instant::now());
                         window::oldest().and_then(|id| window::set_mode(id, Mode::Windowed))
                             
                     } else {
-                        println!("{:?} Working...", elapsed);
                         Task::none()
                     }
                     
                 },
                 Breaking(start_time) => {
                     let elapsed = now.duration_since(start_time);
-                    if elapsed > BREAK_DURATION {
+                    if elapsed > app.config.break_duration {
                         // hide mouse
                         app.state = Working(Instant::now());
                         window::oldest().and_then(|id| window::set_mode(id, Mode::Hidden))
@@ -91,11 +159,17 @@ fn update(app: &mut App, message: Message) -> iced::Task<Message> {
                 }
             };
             task
+        },
+        Message::OpenSettings => {
+            Task::none()
+        },
+        Message::CloseSettings => {
+            Task::none()
         }
     }
 }
 
-fn view(_app: &App) -> Element<'_, Message> {
+fn view(_app: &App, window_id: window::Id) -> Element<'_, Message> {
     mouse_area(
         container("Time for a pause!")
         .width(Length::Fill)
